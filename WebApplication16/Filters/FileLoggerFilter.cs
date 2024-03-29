eab12122dd4aa8ab330de8ea154762ff31d8bba1
antonRMNV <anton.rmnv03@gmail.com>
2024-03-29 16:08:45 +0200

    Добавьте файлы проекта.

---
+++ WebApplication16/Filters/FileLoggerFilter.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using WebApplication16.Models;
+
+namespace WebApplication16.Filters
+{
+    public class FileLoggerFilter : Attribute, IAsyncActionFilter
+    {
+        private readonly FileLogger logger;
+
+        public FileLoggerFilter()
+        {
+            this.logger = new FileLogger(@"log.txt");
+        }
+
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            logger.LogInformation($"{context.RouteData.Values["action"] as string} method " +
+                $"executing at {DateTime.Now.ToLongTimeString()}");
+            await next();
+        }
+    }
+}
+
---
+++ WebApplication16/Views/Home/Info.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication16.Views.Home
+{
+    public class InfoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
